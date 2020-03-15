77659ba4656c30d9e147392c70780b3f2f3a1a94
matiasdiez96 <61991768+matiasdiez96@users.noreply.github.com>
2020-03-15 19:15:51 -0300

    carpeta app

---
+++ LibreriaRectorado/app/Http/Controllers/TipoClienteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tipo_cliente;
+use Illuminate\Http\Request;
+
+class TipoClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tipo_cliente  $tipo_cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tipo_cliente $tipo_cliente)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tipo_cliente  $tipo_cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tipo_cliente $tipo_cliente)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tipo_cliente  $tipo_cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tipo_cliente $tipo_cliente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tipo_cliente  $tipo_cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tipo_cliente $tipo_cliente)
+    {
+        //
+    }
+}
