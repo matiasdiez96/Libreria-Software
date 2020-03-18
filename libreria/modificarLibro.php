bb9c151166afedde0b20fc223dfd2fa54679446d
GinoB96 <62030058+GinoB96@users.noreply.github.com>
2020-03-18 20:24:21 -0300

    adaptado a laravel

--- libreria/modificarLibro.php
+++ libreria/modificarLibro.php
@@ -1,108 +1,92 @@
-<?php
-    include("layout.php");
-?>
+@include('layout')
 
 		<div class="titulo">
             <h1>Modificar libro</h1>
-            <img src="imagenes/book.png" alt="logo">
+            <img src="{{asset('storage/book.png')}}" alt="logo">
         </div>
         <div class="container" style="margin-top: 5px;margin-bottom: -15px">
 	        <form>
 	        	<div class="input-group mb-3">
 				  <input type="text" class="form-control" placeholder="Ingrese código del libro..">
 				  <div class="input-group-append">
-				    <button class="btn btn-success" type="submit" data-toggle="modal" data-target="#myModal">Buscar</button>
+				    <button class="btn btn-success" type="submit">Buscar</button>
 				  </div>
 				</div>
 	        </form>
         </div>
-        <form>
-        	<!--
-				Una vez encontrado el libro, por ajax, se desplegarian los input que se muestran debajo pre-cargados
-			-->
-            <div class="container">
-                <form action="/action_page.php">
-                <div class="row">
-                  <div class="col-25">
-                    <label for="id">ISBN - ISSN</label>
-                  </div>
-                  <div class="col-75">
-                    <input type="text" id="id" name="id">
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-25">
-                    <label for="titulo">Titulo</label>
-                  </div>
-                  <div class="col-75">
-                    <input type="text" id="titulo" name="titulo">
-                  </div>
-                </div>
-                <!--
-                <div class="row">
-                  <div class="col-25">
-                    <label for="ed">Ed</label>
-                  </div>
-                  <div class="col-75">
-                    <select id="ed" name="ed">
-                      <option value="">VM</option>
-                      <option value="">LP</option>
-                      <option value="">FEP</option>
-                      <option value="">UNS</option>
-                      <option value="">R.IV</option>
-                      <option value="">ER</option>
-                      <option value="">Ros</option>
-                      <option value="">SM</option>
-                      <option value="">Cuy</option>
-                    </select>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-25">
-                    <label for="auto">Autor</label>
-                  </div>
-                  <div class="col-75">
-                    <input type="text" id="autor" name="autor" placeholder="Autor del libro..">
-                  </div>
-                </div>
-            	-->
-                <div class="row">
-                  <div class="col-25">
-                    <label for="editorial">Editorial</label>
-                  </div>
-                  <div class="col-75">
-                    <input type="text" id="editorial" name="editorial">
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-25">
-                    <label for="Deposito">Deposito</label>
-                  </div>
-                  <div class="col-75">
-                    <input type="text" id="deposito" name="deposito">
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-25">
-                    <label for="general">Precio general</label>
-                  </div>
-                  <div class="col-75" style="display: flex;">
-                    <h1>$</h1><input type="text" id="general" name="general">
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-25">
-                    <label for="estudiante">Precio estudiante</label>
-                  </div>
-                  <div class="col-75" style="display: flex;">
-                    <h1>$</h1><input type="text" id="estudiante" name="estudiante">
-                  </div>
-                </div>
-                <div align="right" style="margin-top: 10px;">
-                  <input type="submit" value="Agregar" style="background: #e86a04;" >
-                </div>
-            </div>
-        </form>
+
+        <div id="ele-1" name="ele-1" style="display: none;">
+        	<form>
+	        	<!--
+					Una vez encontrado el libro, por ajax, se desplegarian los input que se muestran debajo pre-cargados
+				-->
+	            <div class="container">
+	                <form action="/action_page.php">
+	                <div class="row">
+	                  <div class="col-25">
+	                    <label for="id">ISBN - ISSN</label>
+	                  </div>
+	                  <div class="col-75">
+	                    <input type="text" id="id" name="id">
+	                  </div>
+	                </div>
+	                <div class="row">
+	                  <div class="col-25">
+	                    <label for="titulo">Titulo</label>
+	                  </div>
+	                  <div class="col-75">
+	                    <input type="text" id="titulo" name="titulo">
+	                  </div>
+	                </div>
+	                <div class="row">
+	                  <div class="col-25">
+	                    <label for="editorial">Editorial</label>
+	                  </div>
+	                  <div class="col-75">
+	                    <input type="text" id="editorial" name="editorial">
+	                  </div>
+	                </div>
+	                <div class="row">
+	                  <div class="col-25">
+	                    <label for="Deposito">Depósito</label>
+	                  </div>
+	                  <div class="col-75">
+	                    <input type="text" id="deposito" name="deposito">
+	                  </div>
+	                </div>
+	                <div class="row">
+	                  <div class="col-25">
+	                    <label for="general">Precio general</label>
+	                  </div>
+	                  <div class="col-75" style="display: flex;">
+	                    <h1>$</h1><input type="text" id="general" name="general">
+	                  </div>
+	                </div>
+	                <div class="row">
+	                  <div class="col-25">
+	                    <label for="estudiante">Precio estudiante</label>
+	                  </div>
+	                  <div class="col-75" style="display: flex;">
+	                    <h1>$</h1><input type="text" id="estudiante" name="estudiante">
+	                  </div>
+	                </div>
+	                <div align="right" style="margin-top: 10px;">
+	                  <input type="submit" value="Guardar" style="background: #e86a04;" >
+	                </div>
+	            </div>
+	        </form>
+        </div>
+
+        <div id="ele-0" name="ele-0" style="display: none;">
+        	<div class="container">
+        		<div align="center">
+        			<div class="alert alert-danger">
+					  <strong> El libro que intenta buscar no se encuentra en la Base de Datos</strong>
+					</div>
+        		</div>
+        	</div>
+        </div>
+        
         <br><br>
     </body>
 </html>
